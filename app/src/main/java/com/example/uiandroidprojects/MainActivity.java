ecb6c655b2800dc8d7fa400ce65ab85179c52c22
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-11-14 10:46:58 -0300

    Ideia da pagina principal

--- app/src/main/java/com/example/uiandroidprojects/MainActivity.java
+++ app/src/main/java/com/example/uiandroidprojects/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Criar uma lista para acessar cada menu - Essa pagina sera uma pagina de acesso aos projetos */
     }
 }
