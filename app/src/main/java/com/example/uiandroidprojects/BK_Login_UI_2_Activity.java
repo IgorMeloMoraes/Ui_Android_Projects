52cb3f7dd868e5b522d234e5e4ada63115b15ee4
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-11-22 17:36:35 -0300

    Projeto II - BK Food - Criação da Activity e Inserção de Arquivos

---
+++ app/src/main/java/com/example/uiandroidprojects/BK_Login_UI_2_Activity.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BK_Login_UI_2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bk_login_ui2);
+    }
+}
