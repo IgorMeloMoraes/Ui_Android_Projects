b5d052a88e7cb03950b8fc5698799fa6c6359ab8
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-11-22 17:20:54 -0300

    Projeto I - King Cookie - Telas de Login, Cadastro - Variaveis inseridas

---
+++ app/src/main/java/com/example/uiandroidprojects/UI_1_Cadastro_KingCookie.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UI_1_Cadastro_KingCookie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ui1_food_shop);
+    }
+}
---
+++ app/src/main/java/com/example/uiandroidprojects/UI_1_Login_KingCookie.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UI_1_Login_KingCookie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ui1_food_shop);
+    }
+}
