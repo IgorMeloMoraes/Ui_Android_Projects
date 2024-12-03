b6c7de34859bc7ae8ed84a203e08bbd8e698cfda
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-12-03 15:15:24 -0300

    Projeto V - UI Dashboard - Finalização da UI

---
+++ app/src/main/java/com/example/uiandroidprojects/UI_5_Dashboard_Activity.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UI_5_Dashboard_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui5_dashboard);
+    }
+}
