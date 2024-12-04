140cc7435f117094462ff5b00c333b212e7d8901
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-12-04 12:44:54 -0300

    Projeto IX - UI Login - Finalização da UI

---
+++ app/src/main/java/com/example/uiandroidprojects/UI_9_Login_Activity.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UI_9_Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui9_login);
+    }
+}
