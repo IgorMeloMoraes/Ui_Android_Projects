6b9431225214ee22b6443e0b37f113a417d29b82
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-12-04 12:01:28 -0300

    Projeto VIII - UI Login e Cadastro - Finalização da UI

---
+++ app/src/main/java/com/example/uiandroidprojects/UI_8_Cadastro_Activity.java
@@ -0,0 +1,14 @@
+package com.example.uiandroidprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UI_8_Cadastro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui8_cadastro);
+    }
+}
